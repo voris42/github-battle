c29e704a5a8ae72cce2d591eaf8e207098f71494
voris42 <boris.muminovic@gmail.com>
2018-08-21 18:32:52 +0200

    Refactored Popular.js with stateless functional components

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,30 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage(props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="languages">
+      {languages.map(function(lang) {
+        return (
+          <li
+            key={lang}
+            onClick={props.onSelect.bind(null, lang)}
+            className={lang === props.selectedLanguage ? 'highlighted' : null}
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component {
   constructor(props) {
@@ -20,25 +46,12 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
     return (
       <div>
-        <ul className="languages">
-          {languages.map(function(lang) {
-            return (
-              <li
-                key={lang}
-                onClick={this.updateLanguage.bind(null, lang)}
-                className={
-                  lang === this.state.selectedLanguage ? 'highlighted' : null
-                }
-              >
-                {lang}
-              </li>
-            );
-          }, this)}
-        </ul>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
       </div>
     );
   }
